886271b66042f4335ee26605a254baba0f3f0b08
Kumar Vijay <vijay81238@gmail.com>
2024-12-03 23:33:09 +0530

    initial commit

---
+++ src/main/java/com/vijay/vz/dreamshop/model/Category.java
@@ -0,0 +1,25 @@
+package com.vijay.vz.dreamshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    List<Product> products;
+}
